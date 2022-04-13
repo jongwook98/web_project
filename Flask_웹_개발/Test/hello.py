481f61f63f44d88fa0e162ff5c95de974d011edf
jongwook98 <oucsa1541@naver.com>
2022-04-14 01:31:23 +0900

    index_page update

--- Flask_웹_개발/Test/hello.py
+++ Flask_웹_개발/Test/hello.py
@@ -2,47 +2,46 @@
 #from datetime import datetime
 
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
+#from flask_wtf import FlaskForm
+#from wtforms import StringField, SubmitField
+#from wtforms.validators import InputRequired
 
 from flask_bootstrap import Bootstrap
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[InputRequired()])
-    submit = SubmitField('Submit')
+#class NameForm(FlaskForm):
+#    name = StringField('What is your name?', validators=[InputRequired()])
+#submit = SubmitField('Submit')
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234125'
 bootstrap = Bootstrap(app)
-#bootstrap.config['SECRET_KEY']
-
 #moment = Moment(app)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')#, methods=['GET', 'POST'])
 def index():
-    #name = None
+    '''
+    name = None
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-        #name = form.name.data
         #form.name.data = ''
+        return redirect(url_for('index'))
+        name = form.name.data
+        #form.name.data = ''
+    '''
     #return render_template('index.html', form=form, name=name)
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html')
+   
 
-    #return render_template('index.html', current_time=datetime.now())
-    
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-    #return '<h1>Hello, %s!</h1>' % name
 
 @app.route('/accident/')
 def accident():
@@ -57,6 +56,7 @@ def defective_lane():
 @app.route('/over_speeding/')
 def over_speeding():
     return render_template('over_speeding.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
